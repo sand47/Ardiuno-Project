684d4bc2aaa4834c8b422429705f43495b2cbb9c
Sandeep Kumar <sandeepkumaramni@gmail.com>
2018-02-04 21:03:11 +0530

    Add files via upload

---
+++ ht12e_ht12d/binary_counter_ht12e.ino
@@ -0,0 +1,36 @@
+int ledPin[] = {7,8,9,10};
+
+void setup()
+{
+  for (int i =0;i<4;i++)
+  {
+    pinMode(ledPin[i], OUTPUT);
+  }
+}
+
+void loop() 
+{
+  for (byte counter =0;counter<=15; counter++)
+  {
+    displayBinary(counter);
+    delay(500); 
+  }
+}
+
+void displayBinary(byte numToShow)
+{
+  for (int i =0;i<4;i++)
+  {
+    if (bitRead(numToShow, i)==1)
+    {
+      digitalWrite(ledPin[i], HIGH); 
+    }
+    else
+    {
+      digitalWrite(ledPin[i], LOW); 
+    }
+  }
+
+}
+
+
