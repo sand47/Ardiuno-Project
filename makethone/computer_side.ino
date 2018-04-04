e044921e49c83efc00438912264868e349f21761
Sandeep Kumar <sandeepkumaramni@gmail.com>
2018-04-04 20:28:03 +0530

    Add files via upload

---
+++ makethone/computer_side.ino
@@ -0,0 +1,101 @@
+#include<Mouse.h>
+#include<Keyboard.h>
+
+int st=1;                                                                       //Status of last arrow key used
+int mode=3;                                                                     //Mode 1 - Mouse , 2 - Keyboard , 3 - Disable
+int pr=1;
+# define mo Serial3                                                             //Define Serial 3 as mo
+
+void setup()
+{
+   mo.begin(9600);
+   Serial.begin(9600);
+   pinMode(A0,OUTPUT);                                                          //Set as output pins for led indication
+   pinMode(A1,OUTPUT);
+}
+
+ void nothing()
+{
+                                                                                //Blank function Mode 3 - disable
+}
+
+void kb(char val)                                                               //Keyboard function for Mode 2 - Keyboard
+{
+  Keyboard.begin();                                                             //Start Keyboard data transmission
+  int y = (int(val))%10;
+  if(y==0&&st==2)
+  {
+   Keyboard.press(216);                                                         //Left key press
+     st=1;
+   Keyboard.release(216);
+  }
+  else if(y==9&&st==2)
+  {
+    Keyboard.press(215);                                                        //Right key press
+       st=1;                                                                    
+    Keyboard.release(215);
+  }
+  else if(y==4||y==5)
+   st=2;
+  Keyboard.end();                                                                //End keyboard transmission
+  
+}
+
+void mouse(char val)                                                              //Mouse function for Mode 1 - Mouse
+{
+  Mouse.begin();                                                                  //Mouse begin
+  int x = (int(val))/10;
+  int y = (int(val))%10;
+  x = map(x,0,9,10,-10);
+  y = map(y,0,9,-10,10);
+  Mouse.move(y,x,0);
+  Mouse.end();                                                                     //End mouse 
+}
+
+void loop()
+{
+  while(mo.available())                                                           //Check if data is available
+  {
+    char a = mo.read();
+    
+    if(int(a)==120&&pr!=120)                                                      //Check if a mode change has been called
+     {                                                                            // with character 120
+      if(mode==1)
+      mode=2;                                                                     //Cyclically assign modes
+      else if(mode==2)
+      mode=3;
+      else
+      mode=1;
+     }
+     else if(int(a)==121&&pr!=121)                                                         //Check if button click is requeste
+   {  Mouse.click();
+     delay(500);}     
+     //If true click it
+     else{
+     if(mode==1)
+      kb(a);
+     else if(mode==2)
+      mouse(a);
+     else if(mode==3)
+      nothing();
+     }
+     if(mode==3)                                                                    //If mode 3 , turn off all LEDs
+     {
+       digitalWrite(A0,0);
+       digitalWrite(A1,0);
+     }
+     else if(mode==2)                                                                //For mode 2 turn on one LED
+     {
+       digitalWrite(A0,1);
+       digitalWrite(A1,0);
+     }
+     else if(mode==1)                                                                 //For mode 1 turn on the other LED
+     {
+       digitalWrite(A0,0);
+       digitalWrite(A1,1);
+     }
+     pr = int(a);
+      
+  }
+}
+
---
+++ makethone/main.ino
@@ -0,0 +1,55 @@
+
+#include "I2Cdev.h"
+#include "MPU6050.h"
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+#include "Wire.h"
+#endif
+MPU6050 accelgyro;
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+#define OUTPUT_READABLE_ACCELGYRO
+#define LED_PIN 13
+bool blinkState = false;
+int mode = 3;
+
+void setup() {
+  #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+  Wire.begin();
+  #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+  Fastwire::setup(400, true);
+  #endif
+  Serial.begin(9600);
+  pinMode(8, OUTPUT);                                                                                         
+  digitalWrite(8, HIGH);
+  pinMode(A0, OUTPUT);
+  digitalWrite(A0, HIGH);
+  pinMode(7, INPUT);                                                          //Switch for Mode selection
+  pinMode(14, OUTPUT);                                      
+  pinMode(A1,INPUT);                                                          //Switch for click
+  Serial.println("Initializing I2C devices...");
+  accelgyro.initialize();
+  Serial.println("Testing device connections...");
+  Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+}
+
+void loop() {
+  accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  delay(5);
+  if (digitalRead(7) == 0)                                                 //Check if switch mode button is pressed
+  { 
+      Serial.print(char(120));
+      delay(100);
+   }
+   else if (digitalRead(A1)==0)                                               //Check if click button is pressed
+   {
+      Serial.print(char(121));
+    }
+    else {
+      int x1 = map(ax, -15000, 15000, 0, 9);                                        //Mapping values of gyroscope to mouse X axis
+      int y1 = map(ay, -15000, 15000, 0, 9);                                        //Mapping values of gyroscope to mouse Y axis
+      int z1 = x1 * 10 + y1;                                                        //Converting data to a single character
+      Serial.print(char(z1));                                                       //Sending the data to the other Arduino 
+    
+  }
+
+}
